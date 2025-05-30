3ce26a1bafc8d648f344598ec73299a831a0bb5a
Sonal Shenoy <sonal.shenoy@kellogg.northwestern.edu>
2025-05-30 17:18:28 +0000

    main changes - routes added, sessions and users controllers, sessions/new login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password_digest"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  # explicitly creating login and logout routes
+    get("/login", { :controller => "sessions", :action => "new" })
+    post("/login", { :controller => "sessions", :action => "create" })
+    get("/logout", { :controller => "sessions", :action => "destroy" })
+
 end
