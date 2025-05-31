b9b7ecec7e78f71b275357493313340333fa130f
Sonal Shenoy <sonal.shenoy@kellogg.northwestern.edu>
2025-05-31 00:28:46 +0000

    fixed the username not displaying upon login!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
